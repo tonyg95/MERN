6273c41526f38dba55b0601218d29c6594aad8e6
Tony <Tonyrobertgonzales@gmail.com>
2021-03-15 01:27:17 -0400

    Initial Commit 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res)=> res.send('API running'));
+app.get('/', (req,res)=> res.send('API runnings'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
