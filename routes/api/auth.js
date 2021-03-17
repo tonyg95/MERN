87d2b436a9ca14d9184eaa47239e8a7dbe5f48da
Tony <Tonyrobertgonzales@gmail.com>
2021-03-17 08:48:40 -0400

    add auth and functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('auth route'));
+router.get('/', auth, async (req, res) => {
+try {
+    const user = await User.findById(req.user.id);
+    res.json(user);
+} catch (err) {
+    return res.status(400).json({errors:[{msg: 'Test test'}]});
+}
+
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
 
-const {check, validationResult} = require('express-validator/check');
+const User = require('../../models/User');
 
+const bcrypt = require('bcryptjs');
 // @route post appi/users
 // @desc register user
 // @access public no token
@@ -13,13 +17,49 @@ router.post('/',
     check('email','please include email').isEmail(),
     check('password','please enter password').isLength({min: 6})
 ],
- (req, res) => {
+ async (req, res) => {
     //req.body looks like { name: 'tony', password: 'asdfas' }
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
     }
-    res.send('user route')
+
+    const { name, email, password} = req.body;//grab information from page
+
+    try{
+        let user = await User.findOne({email: email});//search db for same email
+        if(user){
+            return res.status(400).json({errors:[{msg: 'user already exists'}]});
+        }
+
+        user = new User({ //create new model
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);//generate salt
+
+        user.password = await bcrypt.hash(password, salt);//encrypt password
+
+        await user.save();//returns promise that contains
+
+        const payload = { //object with a user that has an id
+            user: {
+                id: user.id
+            }
+        };
+        //generate jwt token with a payload,secretString,options,callback(err,token)
+        jwt.sign(payload,config.get('jwtSecret'),{expiresIn: 360000},
+        (err,token)=> {//inside the callback you get either an error or a token 
+            if(err) throw err;
+            res.json({token});
+        });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
 });
 
 module.exports = router;
